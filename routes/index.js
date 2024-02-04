489d49db4180326c0fc825f47d920398b44fecf3
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2024-02-03 23:44:36 -0700

    Added message that will display when users first navigate to the URL

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
+router.get('/', (req, res) => {
+    res.send('Welcome to my Contact Book API!')
+});
+
 router.use('/contacts', require('./contacts'))
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
