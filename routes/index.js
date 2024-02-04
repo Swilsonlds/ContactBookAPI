44c683952c28c0afb744c0f730254a44c6ac0089
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2024-02-03 23:50:15 -0700

    Modified message that displays when first navigating to the URL

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
 router.get('/', (req, res) => {
-    res.send('Welcome to my Contact Book API!')
+    res.send('Welcome to my Contact Book API!<br><br>Add "/api-docs" to the end of the URL to view all available API requests.')
 });
 
 router.use('/contacts', require('./contacts'))
